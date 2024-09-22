457bfa91395ac2b7a4508f9980bfaab1c15834c2
Awais-404 <155368467+Awais-404@users.noreply.github.com>
2024-09-22 13:54:55 +0500

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,41 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "Basic window");
+    InitWindow(screenWidth, screenHeight, "Ping Pong");
 
     SetTargetFPS(60);
 
-    Rectangle paddle_left{30, (screenHeight/2) - (paddle_left.height/2), 20, 100};
-    Rectangle paddle_right{(screenWidth-30-paddle_right.width), (screenHeight/2) - (paddle_left.height/2), 20, 100, };
-    struct circle{
+    class Paddle{
+    public:
+        float width = 10;
+        float height = 100;
+        float x;
+        float y = (screenHeight/2) - (height/2);
+        Color color = BLACK;
+    public:
+        void draw(){DrawRectangle(x, y, width, height, color);};
+    };
+    Paddle left_paddle; left_paddle.x = 10;
+    Paddle right_paddle; right_paddle.x = (screenWidth-10-right_paddle.width);
+
+    class Ball{
+    public:
         int x = screenWidth/2;
         int y = screenHeight/2;
         float radius = 10;
         Color color = BLACK;
-
+    public:
         void draw(){DrawCircle(x, y, radius, color);}
     };
-    circle ball;
+    Ball ball;
+
     while (!WindowShouldClose())
     {
 
         BeginDrawing();
 
-            DrawRectangleRec(paddle_left, BLACK);
-            DrawRectangleRec(paddle_right, BLACK);
+            left_paddle.draw();
+            right_paddle.draw();
             ball.draw();
 
             ClearBackground(RAYWHITE);
